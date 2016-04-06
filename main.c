b6c7c10dcf9257f8a5f0db6dd3ca48b7ebf4176b
zilong <123@163.com>
2016-04-06 16:04:27 +0800

    update mutex and cond test routine.

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-//#define    NUM_THREADS     16 
 #define    NUM_THREADS     16 
+#define DBG(fmt, ...) do { printf("%s line %d ,"fmt, __func__, __LINE__, ##__VA_ARGS__); } while(0)
 
 typedef struct pthread_private_data
 {
@@ -25,9 +25,10 @@ void *thread_func_entry(void *args)
     while(lock == 0)
     {
 	pthread_cond_wait(cookie->cond, cookie->mutex);
+	DBG("wakeup!\n");
     }
     pthread_mutex_unlock(cookie->mutex);
-    printf("%s line %d, print from thread.cookie -> thread_ino = %ld.\n", __func__, __LINE__, cookie->thread_ino);
+    DBG("print from thread.cookie -> thread_ino = %ld.\n", cookie->thread_ino);
     return (void*)cookie->thread_ino;
 }
  
@@ -40,28 +41,44 @@ int main(void)
  
     for( t = 0; t < NUM_THREADS; t++)
     {
-        printf("%s line %d, creating thread %d.\n", __func__, __LINE__, t);
+        DBG("creating thread %d.\n", t);
 	thread_cookie[t].thread_ino = t;
 	thread_cookie[t].cond = &cond;
 	thread_cookie[t].mutex= &mtx;
         rc = pthread_create(&thread[t], NULL, thread_func_entry, &thread_cookie[t]);
         if (rc)
         {
-            printf("%s line %d, create thread failed, return code is %d\n", __func__, __LINE__, rc);
+            DBG("create thread failed, return code is %d\n", rc);
             return EXIT_FAILURE;
         }
     }
     sleep(10);
+
+#define BROADCAST
+
+#ifdef BROADCAST
     pthread_mutex_lock(&mtx);
     lock = 1;
     pthread_cond_broadcast(&cond);
-    //pthread_cond_signal(&cond);
     pthread_mutex_unlock(&mtx);
+#else 
+    pthread_mutex_lock(&mtx);
+    lock = 1;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mtx);
+	
+    sleep(3);
+    DBG("wakeup next!\n");
+
+    pthread_mutex_lock(&mtx);
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mtx);
+#endif
 
     for( t = 0; t < NUM_THREADS; t++)
     {
         pthread_join(thread[t], &status);
-	printf("%s line %d, return %ld.\n", __func__, __LINE__, (unsigned long)status);
+	DBG("return %ld.\n", (unsigned long)status);
     }
 
     return EXIT_SUCCESS;
