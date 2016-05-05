b279d07ab82f0e9222aa5ac5e4506cbec242a669
czl <13824125580@163.com>
2016-05-05 15:05:47 +0800

    fix

--- android/androidthreadtest/src/com/example/androidthreadtest/MainActivity.java
+++ android/androidthreadtest/src/com/example/androidthreadtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,6 +28,8 @@ public class MainActivity extends Activity implements OnClickListener{
         text = (TextView)findViewById(R.id.text);
         changetext = (Button)findViewById(R.id.change_text);
         changetext.setOnClickListener(this);
+        MyThread mythread = new MyThread();
+        new Thread(mythread).start();
     }
 
     private Handler handler = new Handler() {
@@ -51,14 +54,38 @@ public class MainActivity extends Activity implements OnClickListener{
     		new Thread(new Runnable(){
     			@Override
     			public void run(){
-    				Message message = new Message();
-    				message.what = UPDATE_TEXT;
-    				handler.sendMessage(message);
+    			//	Message message = new Message();
+    			//	message.what = UPDATE_TEXT;
+    			//	handler.sendMessage(message);
     			}
     		}).start();
     		break;
     	 default:
     		 break;
+    	}
+    }
+    
+    class MyThread implements Runnable {
+    	@Override
+    	public void run(){
+    		
+    		while(true){
+				Message message = new Message();
+				message.what = UPDATE_TEXT;
+				handler.sendMessage(message);
+				Log.d("message", "sendmessage");
+				try {                 
+						Thread.sleep(500);                 
+				} 
+				catch (NumberFormatException e) 
+				{                
+					e.printStackTrace();                 
+				} 
+				catch (InterruptedException e) 
+				{                           
+					e.printStackTrace();                 
+				}  
+			}
     	}
     }
 
