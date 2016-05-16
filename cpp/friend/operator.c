4e4a1209e053f20e1812d5c66327e8779813b2fa
13824125580 <13824125580@163.com>
2016-05-16 13:14:59 +0100

    Update operator.c
    test this pointer that when it is null.

--- cpp/friend/operator.c
+++ cpp/friend/operator.c
@@ -1,36 +1,50 @@
 #include <iostream>
 using namespace std;
- 
+
 class complex
 {
 friend istream &operator >> (istream &in,complex &s);
 public:
-    int operator()(int x) { 
+    int operator()(int x) {
         cout<<"operator overload"<<endl;
-	return 2*x;
-    } 
+        return 2*x;
+    }
+
+    int test_this(void)
+    {
+        cout<<"this = "<<this<<endl;
+        return 0;
+    }
 private:
     int real;
     int image;
 };
- 
-istream &operator >> (istream &in,complex &s) 
-{ 
-    cout << "请输入实部的值： " ; 
-    cin >> s.real ; 
+
+istream &operator >> (istream &in,complex &s)
+{
+    cout << "请输入实部的值： " ;
+    cin >> s.real ;
     cout << s.real<<endl;
-    cout << "请输入虚部的值： " ; 
-    cin >> s.image; 
+    cout << "请输入虚部的值： " ;
+    cin >> s.image;
     cout<<s.image<<endl;
-    return in; 
-} 
+    return in;
+}
 
 //定义的复数类的输入运算的重载 
-int main() 
-{ 
-    complex complex1; 
-    cin >> complex1; 
+int main()
+{
+    complex complex1;
+    cin >> complex1;
     cout<<complex1(5)<<endl;
- 
+
+    cout<<"&complex1 = "<<&complex1<<endl;
+
+    complex1.test_this();
+
+    complex *p = NULL;
+
+    p->test_this();
+
     return 0;
-} 
+}
