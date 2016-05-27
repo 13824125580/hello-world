c83c0bba631480ff10195c70f0ceccf48a3e96ec
13824125580 <13824125580@163.com>
2016-05-27 11:03:19 +0100

    test proctected member access.

--- cpp/insidcpp/chapter1/point3d.cpp
+++ cpp/insidcpp/chapter1/point3d.cpp
@@ -1,105 +1,101 @@
 #include <iostream>
 using namespace std;
-
 class point3d
 {
 public:
-	point3d(float x = 0.0, float y = 0.0, float z = 0.0) : _x(x), _y(y), _z(z)
-	{
-		
-	}
-	point3d(const point3d& obj)
-	{
-		cout<<"copy constuctor"<<endl;
-		_x = obj._x;
-		_y = obj._y;
-		_z = obj._z;
-	}
-
-	float x() const
-	{
-		cout<<"funtion ptr"<<endl;
-		return _x;
-	}
-	
-	float y() const
-	{
-		return _y;
-	}
-	
-	float z() const
-	{
-		return _z;
-	}
-
-	void x(float xval)
-	{
-		_x = xval;
-	}
-
-	void y(float yval) 
-	{
-		_y = yval;
-	}
-
-	void z(float zval) 
-	{
-		_z = zval;
-	}
+point3d(float x = 0.0, float y = 0.0, float z = 0.0) : _x(x), _y(y), _z(z)
+{
+}
+point3d(const point3d& obj)
+{
+cout<<"copy constuctor"<<endl;
+_x = obj._x;
+_y = obj._y;
+_z = obj._z;
+}
+float x() const
+{
+cout<<"funtion ptr"<<endl;
+return _x;
+}
+float y() const
+{
+return _y;
+}
+float z() const
+{
+return _z;
+}
+void x(float xval)
+{
+_x = xval;
+}
+void y(float yval)
+{
+_y = yval;
+}
+void z(float zval)
+{
+_z = zval;
+}
 private:
-	float _x;
-	float _y;
-	float _z;
+float _x;
+float _y;
+float _z;
 };
-
 inline ostream& operator<<(ostream &os, const point3d &pt)
 {
-	return os << "(" << pt.x() << ", "
-		<< pt.y() <<", " << pt.z() << ")";
+return os << "(" << pt.x() << ", "
+<< pt.y() <<", " << pt.z() << ")";
 }
-
 class point {
 public:
-	point(float x = 0.0) : _x(x) { }
-	float x() { return _x; }
-	void x(float xval) { _x = xval; }
+point(float x = 0.0) : _x(x) { }
+float x() { return _x; }
+void x(float xval) { _x = xval; }
 protected:
-	float _x;
+float _x;
 };
-
 class point2d : public point {
 public:
-	point2d(float x = 0.0, float y = 0.0) : point(x), _y(y) { }
-	float y() { return _y; }
-	void y(float yval) { _y = yval; }
+point2d(float x = 0.0, float y = 0.0) : point(x), _y(y) { }
+float y() { return _y; }
+void y(float yval) { _y = yval; }
 protected:
-	float _y;
+float _y;
 };
-
 class point3D : public point2d {
 public:
-	point3D(float x = 0.0, float y = 0.0, float z = 0.0) : point2d(x, y), _z(z) { }
-	float z() { return _z; }
-	void z(float zval) { _z = zval; _z = _x; _z = _y;}
+point3D(float x = 0.0, float y = 0.0, float z = 0.0) : point2d(x, y), _z(z) { }
+float z() { return _y; }
+void z(float zval) { _z = zval; _z = _x; _z = _y;}
 protected:
-	float _z;
+float _z;
 };
 
+class point4d : public point2d {
+public:
+     point3D d3;
+     void testfool(void)
+     {
+//	d3._y = 9;	
+	_y = 9;
+     }
+};
 point3d foo(point3d r3d)
 {
-	return r3d;	
+return r3d;
 }
 point3d pd;
-typedef float (point3d::*p)() const;//定义指向Person类无参数无返回值的成员函数的指针  
+typedef float (point3d::*p)() const;//定义指向Person类无参数无返回值的成员函数的指针
+point4d d4;
 int main(void)
 {
-	cout<<pd<<endl;
-	p ip;
-	ip = &point3d::x;
-	(pd.*ip)();
-	
-	point3d cobj = pd;
-
-	point3d ret = foo(cobj);
-	return 0;
+cout<<pd<<endl;
+p ip;
+ip = &point3d::x;
+(pd.*ip)();
+point3d cobj = pd;
+point3d ret = foo(cobj);
+return 0;
 }
