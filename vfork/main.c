ff5e55af4f563d7564e3933bbfc8d52714a2b530
caozilong <13824125580@163.com>
2016-11-04 17:27:31 +0800

    vfork test

---
+++ vfork/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+ 
+int main( void)
+{
+	pid_t pid;
+	int abc = 100;
+	if((pid = vfork())<0){
+		perror("vfork is failed");
+		exit(1);
+	}else if(pid==0){
+		abc = 99;
+		printf("this is child process\n");
+		exit(0);
+	}else{
+		printf("this is parent process, abc = %d.\n", abc);
+		if(wait(NULL)==-1){
+			perror("fail to wait");
+			exit(1);
+		}
+	}
+ 
+	return 0;
+}
+
